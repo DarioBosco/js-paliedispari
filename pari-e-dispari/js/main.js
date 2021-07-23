aae5da702327509edfe4183ce30731a72c0966ec
Dario Bosco <dario_bosco@protonmail.com>
2021-07-23 15:02:19 +0200

    Aggiunta generazione del numero del PC e somma dei due valori

--- pari-e-dispari/js/main.js
+++ pari-e-dispari/js/main.js
@@ -2,24 +2,24 @@
 TODO: 
 //L’utente sceglie pari o dispari e inserisce un numero da 1 a 5.
 //Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione).
-Sommiamo i due numeri
+//Sommiamo i due numeri
 Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione)
 Dichiariamo chi ha vinto.
 */
 
-var inputChoice = '';
-var inputNumber = 0;
-var computerNumber = 0;
+var userChoice;
+var userNumber;
+var computerNumber;
+var sumResult;
+
 /* L’utente sceglie pari o dispari e inserisce un numero da 1 a 5. Eseguo controlli sui valori inseriti.*/
 do {
-	inputChoice = prompt(`Scegli pari o dispari! Pari = 'P' | Dispari = 'D' `).toUpperCase();
-	console.log(inputChoice);
-} while (inputChoice != 'D' && inputChoice != 'P');
+	userChoice = prompt(`Scegli pari o dispari! Pari = 'P' | Dispari = 'D'`).toUpperCase();
+} while (userChoice != 'D' && userChoice != 'P');
 
 do {
-	inputNumber = parseInt(prompt(`Scegli un numero da 1 a 5`));
-	console.log(inputNumber);
-} while (inputNumber < 1 || inputNumber > 5 || isNaN(inputNumber));
+	userNumber = parseInt(prompt(`Scegli un numero da 1 a 5`));
+} while (userNumber < 1 || userNumber > 5 || isNaN(userNumber));
 
 /* Definisco la variabile per la generazione di un numero intero random */
 function getRandomInt(min, max) {
@@ -28,3 +28,5 @@ function getRandomInt(min, max) {
 
 /* Richiamo la funzione appena creata assegnadone il valore a computerNumber */
 computerNumber = getRandomInt(1, 5);
+/* Riassegno a sumResult la somma dei numeri dell'utente e del PC */
+sumResult = userNumber + computerNumber;
